e63b3c99f07b9c0ba855e3a10b74d9ddee48833e
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-10-23 12:22:30 +0200

    example updated

--- build-examples.sh
+++ build-examples.sh
@@ -2,3 +2,5 @@
 
 elm-make example/Example0.elm --output=example/Example0.html
 elm-make example/Example1.elm --output=example/Example1.html
+elm-make example/LambdaCalc.elm --output=example/LambdaCalc.html
+elm-make example/Evaluate.elm --output=example/Evaluate.html
